d84a67d0d2aaff11d4f8ae13a8b63f3bb9512437
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-09-25 18:41:28 +0200

    Fixed size canvas as the server expects a given size anyway

--- demos/web/js/openface-demo.js
+++ demos/web/js/openface-demo.js
@@ -56,11 +56,13 @@ function sendFrameLoop() {
 
     if (tok > 0) {
         var canvas = document.createElement('canvas');
-        canvas.width = vid.width;
-        canvas.height = vid.height;
+        var w = 400;//vid.videoWidth;
+        var h = 300;//vid.videoHeight;
+        canvas.width = w;
+        canvas.height = h;
         var cc = canvas.getContext('2d');
-        cc.drawImage(vid, 0, 0, vid.width, vid.height);
-        var apx = cc.getImageData(0, 0, vid.width, vid.height);
+        cc.drawImage(vid, 0, 0, w, h);
+        var apx = cc.getImageData(0, 0, w, h);
 
         var dataURL = canvas.toDataURL('image/jpeg', 0.6)
 
